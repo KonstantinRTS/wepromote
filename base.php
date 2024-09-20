b706a307ea16dd7846a0f987ba7cb6461ca8b85c
konstantinrts <k.developer@acceptance.ltd>
2024-09-20 14:02:34 +0300

    all files 14

--- base.php
+++
@@ -1,8 +0,0 @@
-<section class="contact-last">
-    <div class="contact-last__container _container">
-        <div class="contact-last__body wow fadeInUp" style="animation-delay: 0.3s;">
-            <h2 class="contact-last__title wow fadeInUp" style="animation-delay: 0.4s;">Your growth starts with <br> a conversation.</h2>
-            <div class="contact-last__text wow fadeInUp" style="animation-delay: 0.5s;">Let's make it happen.</div>
-        </div>
-    </div>
-</section>
